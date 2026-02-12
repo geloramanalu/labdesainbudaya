970110c22ba8d0f75164e44819fd82c9138718d6
geloramanalu <gdmanalu@gmail.com>
2026-02-12 04:13:24 +0000

    Deploying to output from @ geloramanalu/labdesainbudaya@45a4849b1760d49602578deb6ac7956ec7a373b8 🚀

---
+++ _next/static/S_1K0TV6aRdIr0dZBLmjj/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdesa\u002Farchive\u002F[slug]","\u002Fdesa\u002Fcraftsmen\u002F[slug]","\u002Fdesa\u002Fevents\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
